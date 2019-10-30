29a7c14f8856b54b8af7ea85b1c3cf52c56349c6
alastor <micaelalastor@gmail.com>
2019-10-30 19:08:47 +0300

    added another test file and test

--- doctotext_test.go
+++ doctotext_test.go
@@ -21,6 +21,17 @@ func TestDocToText(t *testing.T) {
 	fmt.Println(text)
 }
 
+func TestDocToText2(t *testing.T) {
+	path := filepath.Join("testdata", "lefthand.doc")
+	file, err := os.Open(path)
+	defer file.Close()
+	text, err := DocToText(file)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(text)
+}
+
 func TestMscfbDoc(t *testing.T) {
 	path := filepath.Join("testdata", "file-sample_100kB.doc")
 	file, err := os.Open(path)
